12abf0e8b50bbfd1760d8cf44636bbdcc06ca4a8
Jose Angel <jose.angel.hdz.rda@gmail.com>
2023-12-07 13:53:40 -0600

    nuevos scripts

--- tareas/85idiomaeuropa-230715-094956.sql
+++ tareas/85idiomaeuropa-230715-094956.sql
@@ -10,6 +10,7 @@ select * from continent;
 
 Select * from "language";
 SELECT
+    COUNT(*) AS total,
     "language".code,
     "language"."name"
 FROM
@@ -19,15 +20,22 @@ FROM
     INNER JOIN continent continent ON continent.code = country.continent
 WHERE
     countrylanguage.isofficial = true
-    AND continent."name" LIKE '%Europe%'; 
-
-
-
-
-
--- Listado de todos los países cuyo idioma oficial es el más hablado de Europa 
+    AND continent."name" LIKE '%Europe%'
+GROUP BY
+    "language"."code"
+ORDER BY
+    total DESC
+LIMIT
+    1;
+-- Listado de todos los países cuyo idioma oficial es el más hablado de Europa
 -- (no hacer subquery, tomar el código anterior)
-
+SELECT
+    country."name"
+FROM
+    country
+    INNER JOIN countrylanguage countrylanguage ON country.code = countrylanguage.countrycode
+WHERE
+    countrylanguage.languagecode = 135;
 
 
 
