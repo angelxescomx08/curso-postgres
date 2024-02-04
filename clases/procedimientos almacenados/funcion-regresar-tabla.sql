fa1de4588585924616f5a3ffb74822df879789e6
Jose Angel <jose.angel.hdz.rda@gmail.com>
2024-02-04 17:33:56 -0600

    FUNCION QUE REGRESA UNA TABLA

---
+++ clases/procedimientos almacenados/funcion-regresar-tabla.sql
@@ -0,0 +1,13 @@
+CREATE OR REPLACE FUNCTION country_region()
+RETURNS TABLE (id CHARACTER(2), name VARCHAR(40), region VARCHAR(25))
+AS $$
+	--DECLARE
+	BEGIN
+		RETURN QUERY
+		SELECT country_id, country_name, region_name FROM countries
+		INNER JOIN regions ON countries.region_id = regions.region_id;
+	END;
+$$
+LANGUAGE plpgsql;
+
+SELECT * FROM country_region();
